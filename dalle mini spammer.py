e6fe7ff3905d81c7afb4ac3c0e093c26d3b87331
Brock <brockrbrown@gmail.com>
2022-06-26 22:10:43 -0400

    Created main function loop

--- dalle mini spammer.py
+++ dalle mini spammer.py
@@ -24,91 +24,95 @@ picture_locations = (
     (1224, 1011)
     )
 
-query = input("Pick yer poison > ")
-if query == 'q':
-    quit()
-time_to_spend = float(input("How many minutes? > "))
-end = time() + time_to_spend * 60
-url = 'https://huggingface.co/spaces/dalle-mini/dalle-mini'
-tabs = 0
+def main ():
+    query = input("Pick yer poison > ")
+    if query == 'q':
+        quit()
+    time_to_spend = float(input("How many minutes? > "))
+    end = time() + time_to_spend * 60
+    url = 'https://huggingface.co/spaces/dalle-mini/dalle-mini'
+    tabs = 0
 
-folder_locations = [f"{query}", f"{query}\\best", f"{query}\\worst", f"{query}\\best\\cream_of_the_crop",
-                    f"{query}\\best\\participation_prize"]
+    folder_locations = [f"{query}", f"{query}\\best", f"{query}\\worst", f"{query}\\best\\cream_of_the_crop",
+                        f"{query}\\best\\participation_prize"]
 
-for folder_location in folder_locations:
-    if not os.path.exists(f"image_library\\{folder_location}"):
-        os.makedirs(f"image_library\\{folder_location}")
+    for folder_location in folder_locations:
+        if not os.path.exists(f"image_library\\{folder_location}"):
+            os.makedirs(f"image_library\\{folder_location}")
 
-save_location = os.getcwd() + f"\\image_library\\{query}"
+    save_location = os.getcwd() + f"\\image_library\\{query}"
 
-while time() < end:
-    # open the site
-    click_if_exists("luigi_chrome.png")
-    hotkey("ctrl", "t")
-    tabs += 1
+    while time() < end:
+        # open the site
+        click_if_exists("luigi_chrome.png")
+        hotkey("ctrl", "t")
+        tabs += 1
+        sleep(1)
+        copy(url)
+        hotkey("ctrl", "v")
+        hotkey("enter")
+        # enter the query
+        copy(query)
+        sleep(3)
+        moveTo(782, 410)
+        click()
+        '''
+        while not click_if_exists("search_bar.png"):
+            print("looking for the search bar...")
+            pass
+        '''
+        sleep(1)
+        hotkey("ctrl", "v")
+        moveTo(1321, 420)
+        click()
+    print("done with main part")
     sleep(1)
-    copy(url)
-    hotkey("ctrl", "v")
-    hotkey("enter")
-    # enter the query
-    copy(query)
-    sleep(3)
-    moveTo(782, 410)
-    click()
-    '''
-    while not click_if_exists("search_bar.png"):
-        print("looking for the search bar...")
+    for i in range(tabs - 1):
+        sleep(0.5)
+        hotkey("ctrl", "shift", "tab")
+    while found("minimized_container.png"):
         pass
-    '''
-    sleep(1)
-    hotkey("ctrl", "v")
-    moveTo(1321, 420)
-    click()
-print("done with main part")
-sleep(1)
-for i in range(tabs - 1):
+
+    count = 0
+    # copy(f"{query}_{round(random() * pow(10, 10))}")
+
+    for i in range(tabs):
+        if not found("too_much_traffic.png"):
+            while found("minimized_container.png"):
+                pass
+            for index, coordinates in enumerate(picture_locations):
+                x, y = coordinates
+                moveTo(x, y)
+                click(button = 'right')
+                sleep(0.1)
+                for i in range(3):
+                    press("down")
+                press("enter")
+                grabbed_image = False
+                while not grabbed_image:
+                    try:
+                        sleep(0.2)
+                        image = ImageGrab.grabclipboard()
+                        image.save(f"image_library\\{query}\\{query} {round(random() * pow(10, 10))}.png", "PNG")
+                        grabbed_image = True
+                    except OSError as e:
+                        print("Couldn't grab the image, trying again:", e)
+                        click(button = 'right')
+                        sleep(0.5)
+                        for i in range(3):
+                            press("down")
+                        press("enter")
+                        sleep(0.5)
+                count += 1
+            sleep(0.1)
+            hotkey("ctrl", "tab")
     sleep(0.5)
     hotkey("ctrl", "shift", "tab")
-while found("minimized_container.png"):
-    pass
+    for i in range(tabs):
+        hotkey("ctrl", "w")
+        sleep(0.2)
+    subprocess.Popen(r'explorer /select,"{1}\image_library\{0}"'.format(query, os.getcwd()))
 
-count = 0
-# copy(f"{query}_{round(random() * pow(10, 10))}")
-
-for i in range(tabs):
-    if not found("too_much_traffic.png"):
-        while found("minimized_container.png"):
-            pass
-        for index, coordinates in enumerate(picture_locations):
-            x, y = coordinates
-            moveTo(x, y)
-            sleep(0.1)
-            click(button = 'right')
-            sleep(0.3)
-            for i in range(3):
-                press("down")
-            press("enter")
-            grabbed_image = False
-            while not grabbed_image:
-                try:
-                    sleep(0.5)
-                    image = ImageGrab.grabclipboard()
-                    image.save(f"image_library\\{query}\\{query} {round(random() * pow(10, 10))}.png", "PNG")
-                    grabbed_image = True
-                except OSError as e:
-                    print("Couldn't grab the image, trying again:", e)
-                    click(button = 'right')
-                    sleep(0.5)
-                    for i in range(3):
-                        press("down")
-                    press("enter")
-                    sleep(0.5)
-            count += 1
-        sleep(0.5)
-        hotkey("ctrl", "tab")
-sleep(0.5)
-hotkey("ctrl", "shift", "tab")
-for i in range(tabs):
-    hotkey("ctrl", "w")
-    sleep(0.2)
-subprocess.Popen(r'explorer /select,"{1}\image_library\{0}"'.format(query, os.getcwd()))
+if __name__ == "__main__":
+    while True:
+        main()
